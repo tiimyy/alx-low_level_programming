79845d6e06bc1a69125838cc665baad1de74b514
tiimyy <danieltimileyin00@gmail.com>
2023-02-17 14:01:48 -0800

    hello

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,17 +2,17 @@
 /**
 *main - Entry point
 * declared a variable using char
-*Return :0
+*Return: Always 0 (Success/correct)
 */
 int main(void)
 {
 	char ch = 'a';
 
-	while(ch <= 'z')
+	while (ch <= 'z')
 	{
 	putchar(ch);
 	ch++;
-	}	
+	}
 	putchar('\n');
 	return (0);
 }
