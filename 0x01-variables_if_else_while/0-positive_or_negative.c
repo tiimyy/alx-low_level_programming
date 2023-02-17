e7206d24fed24aad21239a0fbbb15605d8857540
tiimyy <danieltimileyin00@gmail.com>
2023-02-17 06:35:15 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,7 +6,8 @@
 	 * created Variable n.
 	 * Used conditional statements.
 	 * Description:  this is to check is the variable n is a negative, positive number or zero.
-	 * */
+	 * return: 0 
+	 */
 int main(void)
 {
 	int n;
@@ -15,15 +16,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d is positive\n",n);
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero\n",n);
+		printf("%d is zero\n", n);
 	}
 	else 
 	{
-		printf("%d is negative\n",n);
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
