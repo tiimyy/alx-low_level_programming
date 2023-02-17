93cd9e3bbc32c4bbb5aa5713fdeb21fefbedb8aa
tiimyy <danieltimileyin00@gmail.com>
2023-02-17 15:56:31 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main -Entry point
+ *created variables
+ *Return: 0 always
+ */
+int main (void)
+{
+	char x='a';
+
+	while (x <= 'z')
+	{
+		if (x != 'q' && x != 'e')
+		{
+		putchar(x);
+		}
+	x++;
+	}
+	putchar('\n');
+}
