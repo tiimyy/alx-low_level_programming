1d9b9e93b2c16937ada024a48d166f047c828572
tiimyy <danieltimileyin00@gmail.com>
2023-02-17 13:32:33 -0800

    git push

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+*main - Entry point
+* declared a variable using char
+*Return : 0
+*/
+int main()
+{
+  
+    
+    char ch = 'a';
+
+    while( ch <= 'z')
+    {
+	putchar(ch);
+	    ch++;}
+    putchar(\n);
+    return (0);
+}
