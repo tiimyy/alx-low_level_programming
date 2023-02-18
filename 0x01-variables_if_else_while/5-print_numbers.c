417210606ba42d3106b34538563bf6db58b7887e
tiimyy <danieltimileyin00@gmail.com>
2023-02-18 00:27:20 -0800

    hello boss

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  *main -Entry point
+  *Return: 0 ALWAYS
+ */
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+	printf("%d", x);
+	x++;
+	}
+	printf("\n");
+	return (0);
+}
