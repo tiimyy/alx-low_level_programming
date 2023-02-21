e84098487144784536e8720ab8666e0db29b335e
tiimyy <danieltimileyin00@gmail.com>
2023-02-21 11:35:52 -0800

    6 abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  *_abs - calculate abs values
+  *@n: parameter for n
+  *Return: (n) a value
+  */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+	n = n * -1;
+	return (n);
+	}
+	else
+	{
+	return (n);
+	}
+
+}
