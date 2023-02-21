d75939752eaa39649d763b447dcc8b631d007ad7
tiimyy <danieltimileyin00@gmail.com>
2023-02-21 06:52:15 -0800

    any case

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  *_isalpha - check if Alphabet
+  *@c: accepts an actual parameter;
+  *Return:0 or 1
+  */
+int _isalpha(int c)
+{
+	if
+	(c >= 65 && c <= 112)
+	return (1);
+	else
+	return (0);
+
+}
