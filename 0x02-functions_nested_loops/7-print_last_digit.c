8d94fc5370fae1b0a8ad63423ab5c3f6edd38dd7
tiimyy <danieltimileyin00@gmail.com>
2023-02-21 12:23:51 -0800

    hello

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,7 +8,17 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
+	if (n >= 0)
+	{
 	last_digit = n % 10;
+	_putchar('0' + last_digit);
 	return (last_digit);
+	};
+	else
+	{
+	last_digit = ((-1 * n) % 10);
+	_putchar('0' + last_digit);
+	return (last_digit);
+	};
 
 }
