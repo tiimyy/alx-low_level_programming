afb5ae9b2b54a61fba63b5e4928a7519f2ab223c
tiimyy <danieltimileyin00@gmail.com>
2023-02-21 05:34:25 -0800

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_alphabet - Entry
+*Description: prints a to z
+*Return: Void
+*/
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	while ('a' < 'z')
+	_putchar(a);
+	_putchar('\n');
+}
