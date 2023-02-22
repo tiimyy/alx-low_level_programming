34a15f91f54a2b7dbb880e32d8dd5a6e60ff7e6b
tiimyy <danieltimileyin00@gmail.com>
2023-02-21 16:53:39 -0800

    m

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,7 +22,7 @@ void times_table(void)
 	{
 	_putchar(',');
 	_putchar(' ');
-	_putcha(' ');
+	_putchar(' ');
 	_putchar(k + '0');
 	}
 	else if (k >= 10)
@@ -33,6 +33,7 @@ void times_table(void)
 	_putchar ((k % 10) + '0');
 	}
 	}
-	_putchar('\n);
+	_putchar('\n');
 	}
+
 }
