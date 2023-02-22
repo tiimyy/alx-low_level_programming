23b25a742b6a5dd4b87f5502a26baa94c15aab11
tiimyy <danieltimileyin00@gmail.com>
2023-02-21 18:43:26 -0800

    hello

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,29 +9,28 @@
 
 int main(void)
 {
-    int count = 3; /* this is so because the first 2 members have been given already. My loop will therefore begin from the 3rd one */
+	int count = 3;
 
-    long int first = 1, second = 2;
-    long int next = first + second;
+	long int first = 1, second = 2;
+	long int next = first + second;
 
-    printf("%lu, ", first);
-    printf("%lu, ", second);
+	printf("%lu, ", first);
+	printf("%lu, ", second);
+	while (count <= 50)
+	{
+		if (count == 50)
+		{
+			printf("%lu\n", next);
+		}
+		else
+		{
+			printf("%lu, ", next);
+		}
+		first = second;
+		second = next;
+		next = first + second;
+		count++;
+	}
+	return (0);
 
-    while (count <= 50)
-    {
-        if (count == 50)
-        {
-            printf("%lu\n", next);
-         }
-         else
-         { 
-           printf("%lu, ", next); 
-         }
-        first = second;
-        second = next;
-        next = first + second; 
-        count++;
-    }
-
-    return (0);
 }
