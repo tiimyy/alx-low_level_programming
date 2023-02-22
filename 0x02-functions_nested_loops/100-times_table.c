3bdbf6da4cba466a1680ff8771acc503e525c625
tiimyy <danieltimileyin00@gmail.com>
2023-02-21 18:20:25 -0800

    m

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+  *times_table(void)- prints timetable
+  *Return: Void
+  */
+int print_times_table(int n)
+{
+        int i;
+        int j;
+        int k;
+
+        for (i = 0; i <= n; i++)
+        {
+        for (j = 0; j <= n; j++)
+        {
+        k = j * i;
+        if (j == 0)
+        {
+        _putchar(k + '0');
+        }
+        if (k <= n && j != 0)
+        {
+        _putchar(',');
+        _putchar(' ');
+        _putchar(' ');
+        _putchar(k + '0');
+        }
+        else if (k >= n)
+        {
+        _putchar(',');
+        _putchar(' ');
+        _putchar ((k / n) + '0');
+        _putchar ((k % n) + '0');
+        }
+        }
+        _putchar('\n');
+        }
+
+}
