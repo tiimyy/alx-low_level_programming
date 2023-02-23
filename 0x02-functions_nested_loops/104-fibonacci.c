0a3e01ec74c9ca6d524c4f408b0d92ef5f942f1e
tiimyy <danieltimileyin00@gmail.com>
2023-02-22 19:10:40 -0800

    hello

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,28 +9,24 @@
 
 int main(void)
 {
-        int count = 3;
+	int count = 3;
+	long int total_sum;
 
-        long int first = 1, second = 2;
-        long int next = first + second;
+	long int first = 0, second = 1;
+	long int next = first + second;
 
-        printf("%lu, ", first);
-        printf("%lu, ", second);
-        while (count <= 98)
-        {
-                if (count == 98)
-                {
-                        printf("%lu\n", next);
-                }
-                else
-                {
-                        printf("%lu, ", next);
-                }
-                first = second;
-                second = next;
-                next = first + second;
-                count++;
-        }
-        return (0);
+	while (count > 0 && next <= 4000000)
+	{
+		if (next % 2 == 0)
+		{
+			total_sum += next;
+		}
+		first = second;
+		second = next;
+		next = first + second;
+		count++;
+	}
+	printf("%ld", total_sum);
 
+	return (0);
 }
