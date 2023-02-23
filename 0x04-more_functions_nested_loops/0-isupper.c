6ff4ccf231ae8ce01ce0cb94b777b2b951258f88
tiimyy <danieltimileyin00@gmail.com>
2023-02-23 05:12:53 -0800

    hello

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#includes "main.h"
+/**
+  *_isupper- checks for upper case
+  *@c: paramter for _is upper function
+  *Return: 0 always
+  */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,3 @@
+#include <stdio.h>
+int _isupper(int c);
+int _putchar(char c);
