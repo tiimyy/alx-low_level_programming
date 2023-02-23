5f47a88d73508279e928ae9e1b37bc618521920e
tiimyy <danieltimileyin00@gmail.com>
2023-02-23 05:51:07 -0800

    hyelloo

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+  *print_numbers- print form 0-9
+  *void
+  *returns: void
+  */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		_putchar(i + '0');
+	_putchar('\n');
+}
