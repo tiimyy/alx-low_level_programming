c2297de9ff9fe709871046ff7c5793f9e677dd51
tiimyy <danieltimileyin00@gmail.com>
2023-02-23 12:27:38 -0800

    hello

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,15 +1,17 @@
 #include "main.h"
 /**
-  *-print_diagonal- prints diagonal
+  *print_diagonal- prints diagonal
   *@n: parameter
   *Returns: Void
   */
 void print_diagonal(int n)
 {
 	int i;
+	int j;
 
 	if (n > 0)
-	{for (i = 1; i <= n; i++)
+	{
+		for (i = 1; i <= n; i++)
 	{
 		for (j = 1 j <= i;  ++)
 		{
