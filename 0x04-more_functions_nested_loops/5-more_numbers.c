8e121a16207373a13e2c61476243b3ca324c0e70
tiimyy <danieltimileyin00@gmail.com>
2023-02-23 06:45:28 -0800

    hello

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  *more_numbers- print numbers
+  *Return : Void
+  */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; i <= 14; i++)
+		{
+			_putchar(j  +  '0');
+		}
+		_putchar('\n');
+	}
+}
