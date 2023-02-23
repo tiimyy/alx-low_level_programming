3d11c8af51b3580a9a22e2ceb2626dcd1429bbdf
tiimyy <danieltimileyin00@gmail.com>
2023-02-23 06:55:22 -0800

    help

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,11 +8,11 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j  +  '0');
+			_putchar(j + '0');
 		}
 		_putchar('\n');
 	}
