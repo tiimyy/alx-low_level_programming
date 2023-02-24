9856c1b3573cfb899864792d2ca788018e4dcc51
tiimyy <danieltimileyin00@gmail.com>
2023-02-24 15:30:47 -0800

    push

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+  *main - enrty point
+  *Return: ) always
+  */
+int main(void)
+{
+	int a = 100;
+	int i:
+
+		i=1;
+	while (i <= a)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("fizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("fizz ");
+		}
+		esle if (i % 5 == 0)
+		{
+			if(i < a)
+			printf("Buzz ");
+			else
+			printf("Buzz")
+		}
+		else
+		{
+			printF("%i ", i);
+		}
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
