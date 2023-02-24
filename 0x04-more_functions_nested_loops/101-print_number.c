5720c2d601bb9e81f8d78510517d310f9968b716
tiimyy <danieltimileyin00@gmail.com>
2023-02-24 15:10:01 -0800

    hello

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  *print_number- prints a value
+  *@n: parameter
+  *Return: 0 always
+  */
+void print_number(int n)
+{
+	unsigned int x;
+
+	if (n < 0)
+	{
+		x = -n;
+		_putchar ('-');
+	}
+	else
+	{
+		x = n;
+	}
+	if (x / 10)
+	{
+		print_number(x / 10);
+	}
+	_putchar((x % 10) + '0');
+}
