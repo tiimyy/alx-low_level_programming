b935a30f6a5add5c78ec15891a5987bc77362321
tiimyy <danieltimileyin00@gmail.com>
2023-02-23 19:57:25 -0800

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#includes "main.h"
+/**
+  *print triangle- prints traingle
+  *@size: size of triangle
+  *Return: void
+  */
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int k;
+
+	if (c > 0)
+	{
+		for (i = 1; i < size i++)
+		{
+		for (k = size; k >= i; k++)
+		{
+		_putchar(' ');
+		}
+		for (j = 1; j <= i; j++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,7 +12,7 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar('0' + j);
+			_putchar(j + '0');
 		}
 		if (i < 9)
 		_putchar('\n');
