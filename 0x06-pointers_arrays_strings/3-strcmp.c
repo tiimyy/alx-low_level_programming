082b3326beec40168698f2303a1b684027532837
tiimyy <danieltimileyin00@gmail.com>
2023-03-01 13:37:26 -0800

    hello

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+  *_strcmp - compares two strings
+  *@s1: first string to be compared
+  *@s2: second string to be compared
+  *Return: a value;
+  */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int result;
+
+	for (i = 0; s1[i] != 0 || s2[i] != 0; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			result = s1[i] - s2[i];
+		}
+	}
+	return (result);
+}
