c8614b0fb40bcaaaac5813481475d06899afb807
tiimyy <danieltimileyin00@gmail.com>
2023-03-02 13:30:08 -0800

    to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  *string_toupper- changes to uppercase
+  *@str: takes in str
+  *Return: str
+  */
+char *string_toupper(char *str)
+{
+	int i;
+	int j;
+	int lenght;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+	lenght = j;
+	for (i = 0; i < lenght; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+	}
+	return (str);
+}
