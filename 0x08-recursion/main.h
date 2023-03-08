f37bce013c8bb0539b4c3f6b5d80e412b93b5e8c
tiimyy <danieltimileyin00@gmail.com>
2023-03-08 13:11:06 -0800

    prime

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,3 +4,5 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
+int is_prime_number(int n);
+int is_prime(unsigned int n, unsigned int c);
