128a934ba430a3b00652802627e44587762b7e0d
tiimyy <danieltimileyin00@gmail.com>
2023-03-10 11:08:30 -0800

    hello

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  *main- prints name of file
+  *@arc: 
+  *@argv
+  *Return: 0 always;
+  */
+int main (int argc, char *argv[])
+{
+	printf("%s",argv[0]);
+	return (0);
+}
