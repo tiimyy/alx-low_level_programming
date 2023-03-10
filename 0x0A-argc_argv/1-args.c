2dc8822d05401570431fc2a60b39c1597b5cf075
tiimyy <danieltimileyin00@gmail.com>
2023-03-10 12:02:32 -0800

    hello

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  *main - main function
+  *argc : number of paramter passed
+  *argv: parameter from command line
+  *return : 0 always
+  */
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
