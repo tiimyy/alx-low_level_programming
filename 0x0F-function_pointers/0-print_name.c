42a78f14dd5ac1f14e8347cfdfccbc85990d2a07
tiimyy <danieltimileyin00@gmail.com>
2023-03-22 11:44:22 -0700

    pushing first for the first time

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+  *print_name- prints a name of a person
+  *@name: string
+  *@f: a pointer to a function
+  *Returns: 0 always
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
