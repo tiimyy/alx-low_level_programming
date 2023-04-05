6959a0d4d997ad9211706f76bd08aaf2069c2fbc
tiimyy <danieltimileyin00@gmail.com>
2023-04-05 14:08:26 -0700

    returns the nth node of a linked list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -5,7 +5,7 @@
   *@index: index of the node
   *Return: nth node or NULL
   */
-listint_ *get_nodeint_at_index(listint_t *head, unsigned int index)
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
