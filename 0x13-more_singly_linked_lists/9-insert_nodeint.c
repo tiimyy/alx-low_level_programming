50aae3ee464aae636fbe775563f4b423b194ee35
tiimyy <danieltimileyin00@gmail.com>
2023-04-05 14:57:49 -0700

    deletes the node at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,12 +16,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx != 0)
 	{
-		for (i = 0; i < idx -1 && h !=NULL; i++)
+		for (i = 0; i < idx - 1 && h != NULL; i++)
 		{
 			h = h->next;
 		}
 	}
-	if (h == NULL && idx !=0)
+	if (h == NULL && idx != 0)
 		return (NULL);
 
 	new = malloc(sizeof(listint_t));
