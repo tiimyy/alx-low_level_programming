76ffc938a58fcea63757e886165f309d16e8509b
tiimyy <danieltimileyin00@gmail.com>
2023-05-04 16:09:01 -0700

    first commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ *binary_to_uint- A functions that changes binary number to an unsigned int
+ *@b: the argument the functions reviceves that holds the expected
+    * value to be converted.
+  *Return:The converted number or 0
+  */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int init;
+	int lenght, base;
+
+	if (!b)
+	{
+		return (0);
+	}
+	init = 0;
+	for (lenght = 0; b[lenght] != "\0"; lenght++)
+		;
+
+	for (lenght--, base = 1; length >= 0; lenght--, base *= 2)
+	{
+		if (b[lenght] != '0' && b[lenght] != '1')
+				{
+				return (0);
+				}
+		if (b[lenght] & 1)
+		{
+			init += base;
+		}
+	}
+	return (init);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,5 @@
+#ifndef _MAINHH_
+#define __MAINH
+unsigned int binary_to_unit(const char *b);
+
+#endif
