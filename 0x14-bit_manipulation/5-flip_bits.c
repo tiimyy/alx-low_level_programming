1a9a58af3995c7e3d7554030916bf1526abb86c0
tiimyy <danieltimileyin00@gmail.com>
2023-05-05 15:44:00 -0700

    number 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,7 +9,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int numbits;
 
-	for (numbits = 0; n || m; n >> 1, m >> 1)
+	for (numbits = 0; n || m; n >>= 1, m >>= 1)
 	{
 		if ((n & 1) != (m & 1))
 			numbits++;
