aa2831a2f0f4c1f8b0d6277edcc5a753e27d38c1
tiimyy <danieltimileyin00@gmail.com>
2023-05-05 21:34:36 -0700

    finishing

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * get_endianness- check the endianness
+ * Return: 0 for bif endian or  1 for little endian
+ */
+int get_endiannes(void)
+{
+	unsigned int a;
+	char *c;
+
+	a=1;
+	c=(char *) &a;
+
+	return ((int)*c);
+}
